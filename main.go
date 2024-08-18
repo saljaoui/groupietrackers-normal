586732b6b0d0b22c48363355c621459dea4a8a46
saljaoui <soufian2022ff@gmail.com>
2024-08-18 19:30:57 +0100

    s

--- main.go
+++ main.go
@@ -9,7 +9,7 @@ import (
 )
 
 func main() {
-	port := ":8085"
+	port := ":8080"
 	http.HandleFunc("/", Groupie_tracker.GetDataFromJson)
 	http.HandleFunc("/Artist/{id}", Groupie_tracker.HandlerShowRelation)
 	http.HandleFunc("/styles/", Groupie_tracker.HandleStyle)
